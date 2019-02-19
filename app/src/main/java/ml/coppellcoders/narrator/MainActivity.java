48d12b5a36c54fe28e4621218b6f39a4371f1be3
unknown <bhatta.darshan26@gmail.com>
2019-02-19 16:36:21 -0600

    update

--- app/src/main/java/ml/coppellcoders/narrator/MainActivity.java
+++ app/src/main/java/ml/coppellcoders/narrator/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Hello", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Test", Toast.LENGTH_SHORT).show();
             }
         });
 
