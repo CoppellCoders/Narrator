da077636ba520e3733cee167e65596dde4101772
unknown <bhatta.darshan26@gmail.com>
2019-02-19 16:24:24 -0600

    inital

---
+++ app/src/main/java/ml/coppellcoders/narrator/MainActivity.java
@@ -0,0 +1,27 @@
+package ml.coppellcoders.narrator;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            ImageButton btn = findViewById(R.id.horror);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
